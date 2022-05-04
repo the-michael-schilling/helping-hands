f096597c4a8a31462c4f7c7d2aea88e993a8f809
Michael Schilling <michaelschilling@Mac-mini-von-Michael.local>
2022-05-04 21:09:10 +0200

    chalk doesnt work anymore'

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
- import React, { useState } from 'react';
+ import React, { useState, useEffect } from 'react';
  import {
    SectionList,
    StyleSheet,
@@ -19,7 +19,7 @@
    Platform,
    PlatformColor
  } from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
+// import Icon from 'react-native-vector-icons/Ionicons';
 
 const data = [
   {
@@ -30,31 +30,46 @@ const data = [
         id: 'Beratungsstelle gegen sexualiesierte Gewalt',
         number: '0531 2336666',
         email: 'info@trau-dich-bs.de',
-        openMonday: '9.30 – 11.30',
-        openTuesday: '9.30 – 11.30',
-        openWednesday: '9.30 – 11.30',
-        openThursday: '9.30 – 11.30',
-        openFriday: '9.30 – 11.30',
+        opensMonday: '-',
+        closesMonday: '-',
+        opensTuesday: '9.30',
+        closesTuesday: '11.30',
+        opensWednesday: '15.00',
+        closesWednesday: '17.00',
+        opensThursday: '9.30',
+        closesThursday: '11.30',
+        opensFriday: '9.30',
+        closesFriday: '11.30',
       },
       {
         id: 'Beratungsstelle gegen sexualiesierte Gewalt',
         number: '0531 2336666',
         email: 'info@trau-dich-bs.de',
-        openMonday: '9.30 – 11.30',
-        openTuesday: '9.30 – 11.30',
-        openWednesday: '9.30 – 11.30',
-        openThursday: '9.30 – 11.30',
-        openFriday: '9.30 – 11.30',
+        opensMonday: '-',
+        closesMonday: '-',
+        opensTuesday: '9.30',
+        closesTuesday: '11.30',
+        opensWednesday: '15.00',
+        closesWednesday: '17.00',
+        opensThursday: '9.30',
+        closesThursday: '11.30',
+        opensFriday: '9.30',
+        closesFriday: '11.30',
       },
       {
         id: 'Beratungsstelle gegen sexualiesierte Gewalt',
         number: '0531 2336666',
         email: 'info@trau-dich-bs.de',
-        openMonday: '9.30 – 11.30',
-        openTuesday: '9.30 – 11.30',
-        openWednesday: '9.30 – 11.30',
-        openThursday: '9.30 – 11.30',
-        openFriday: '9.30 – 11.30',
+        opensMonday: '-',
+        closesMonday: '-',
+        opensTuesday: '9.30',
+        closesTuesday: '11.30',
+        opensWednesday: '15.00',
+        closesWednesday: '17.00',
+        opensThursday: '9.30',
+        closesThursday: '11.30',
+        opensFriday: '9.30',
+        closesFriday: '11.30',
       },
     ]
   },
@@ -66,31 +81,31 @@ const data = [
         id: 'Beratungsstelle gegen sexualiesierte Gewalt',
         number: '0531 2336666',
         email: 'info@trau-dich-bs.de',
-        openMonday: '9.30 – 11.30',
-        openTuesday: '9.30 – 11.30',
-        openWednesday: '9.30 – 11.30',
-        openThursday: '9.30 – 11.30',
-        openFriday: '9.30 – 11.30',
+        opensMonday: '-',
+        closesMonday: '-',
+        opensTuesday: '9.30',
+        closesTuesday: '11.30',
+        opensWednesday: '15.00',
+        closesWednesday: '17.00',
+        opensThursday: '9.30',
+        closesThursday: '11.30',
+        opensFriday: '9.30',
+        closesFriday: '11.30',
       },
       {
         id: 'Beratungsstelle gegen sexualiesierte Gewalt',
         number: '0531 2336666',
         email: 'info@trau-dich-bs.de',
-        openMonday: '9.30 – 11.30',
-        openTuesday: '9.30 – 11.30',
-        openWednesday: '9.30 – 11.30',
-        openThursday: '9.30 – 11.30',
-        openFriday: '9.30 – 11.30',
-      },
-      {
-        id: 'Beratungsstelle gegen sexualiesierte Gewalt',
-        number: '0531 2336666',
-        email: 'info@trau-dich-bs.de',
-        openMonday: '9.30 – 11.30',
-        openTuesday: '9.30 – 11.30',
-        openWednesday: '9.30 – 11.30',
-        openThursday: '9.30 – 11.30',
-        openFriday: '9.30 – 11.30',
+        opensMonday: '-',
+        closesMonday: '-',
+        opensTuesday: '9.30',
+        closesTuesday: '11.30',
+        opensWednesday: '15.00',
+        closesWednesday: '17.00',
+        opensThursday: '9.30',
+        closesThursday: '11.30',
+        opensFriday: '9.30',
+        closesFriday: '11.30',
       },
     ]
   },
@@ -102,31 +117,46 @@ const data = [
         id: 'Sprachschule Braunschweig',
         number: '0151 17988128',
         email: 'info@sprachschule-braunschweig.de',
-        openMonday: '9.30 – 11.30',
-        openTuesday: '9.30 – 11.30',
-        openWednesday: '9.30 – 11.30',
-        openThursday: '9.30 – 11.30',
-        openFriday: '9.30 – 11.30',
+        opensMonday: '-',
+        closesMonday: '-',
+        opensTuesday: '9.30',
+        closesTuesday: '11.30',
+        opensWednesday: '15.00',
+        closesWednesday: '17.00',
+        opensThursday: '9.30',
+        closesThursday: '11.30',
+        opensFriday: '9.30',
+        closesFriday: '11.30',
       },
       {
         id: 'Berlitz Language Center',
         number: '0531 125313',
         email: 'info@berlitz.de',
-        openMonday: '9.30 – 11.30',
-        openTuesday: '9.30 – 11.30',
-        openWednesday: '9.30 – 11.30',
-        openThursday: '9.30 – 11.30',
-        openFriday: '9.30 – 11.30',
+        opensMonday: '-',
+        closesMonday: '-',
+        opensTuesday: '9.30',
+        closesTuesday: '11.30',
+        opensWednesday: '15.00',
+        closesWednesday: '17.00',
+        opensThursday: '9.30',
+        closesThursday: '11.30',
+        opensFriday: '9.30',
+        closesFriday: '11.30',
       },
       {
         id: 'inlingua Braunschweig',
         number: '0531 70221750',
         email: 'sprachen@inlingua-braunschweig.de',
-        openMonday: '9.30 – 11.30',
-        openTuesday: '9.30 – 11.30',
-        openWednesday: '9.30 – 11.30',
-        openThursday: '9.30 – 11.30',
-        openFriday: '9.30 – 11.30',
+        opensMonday: '-',
+        closesMonday: '-',
+        opensTuesday: '9.30',
+        closesTuesday: '11.30',
+        opensWednesday: '15.00',
+        closesWednesday: '17.00',
+        opensThursday: '9.30',
+        closesThursday: '11.30',
+        opensFriday: '9.30',
+        closesFriday: '11.30',
       },
     ]
   },
@@ -138,11 +168,16 @@ const data = [
         id: 'Beratungsstelle gegen sexualiesierte Gewalt',
         number: '0531 2336666',
         email: 'info@trau-dich-bs.de',
-        openMonday: '9.30 – 11.30',
-        openTuesday: '9.30 – 11.30',
-        openWednesday: '9.30 – 11.30',
-        openThursday: '9.30 – 11.30',
-        openFriday: '9.30 – 11.30',
+        opensMonday: '-',
+        closesMonday: '-',
+        opensTuesday: '9.30',
+        closesTuesday: '11.30',
+        opensWednesday: '15.00',
+        closesWednesday: '17.00',
+        opensThursday: '9.30',
+        closesThursday: '11.30',
+        opensFriday: '9.30',
+        closesFriday: '11.30',
       }
     ]
   },
@@ -154,11 +189,16 @@ const data = [
         id: 'Beratungsstelle gegen sexualiesierte Gewalt',
         number: '0531 2336666',
         email: 'info@trau-dich-bs.de',
-        openMonday: '9.30 – 11.30',
-        openTuesday: '9.30 – 11.30',
-        openWednesday: '9.30 – 11.30',
-        openThursday: '9.30 – 11.30',
-        openFriday: '9.30 – 11.30',
+        opensMonday: '-',
+        closesMonday: '-',
+        opensTuesday: '9.30',
+        closesTuesday: '11.30',
+        opensWednesday: '15.00',
+        closesWednesday: '17.00',
+        opensThursday: '9.30',
+        closesThursday: '11.30',
+        opensFriday: '9.30',
+        closesFriday: '11.30',
       },
     ]
   },
@@ -169,11 +209,16 @@ const data = [
       { id: 'Beratungsstelle gegen sexualiesierte Gewalt',
         number: '0531 2336666',
         email: 'info@trau-dich-bs.de',
-        openMonday: '9.30 – 11.30',
-        openTuesday: '9.30 – 11.30',
-        openWednesday: '9.30 – 11.30',
-        openThursday: '9.30 – 11.30',
-        openFriday: '9.30 – 11.30',
+        opensMonday: '-',
+        closesMonday: '-',
+        opensTuesday: '9.30',
+        closesTuesday: '11.30',
+        opensWednesday: '15.00',
+        closesWednesday: '17.00',
+        opensThursday: '9.30',
+        closesThursday: '11.30',
+        opensFriday: '9.30',
+        closesFriday: '11.30',
       }
     ]
   }
@@ -184,7 +229,6 @@ const SectionHeader = ({section }) => {
   // if section is first section add image
   const title = section.title;
   const isFirstSection = section.firstSection;
-
 
   return (
     <View style={styles.sectionHeader}>
@@ -217,7 +261,134 @@ const Contact = ({item}) => {
 
   const [isOpen, setIsOpen] = useState(true);
   const name = item.id;
-  // Hello, I am { name }, and I am {isOpen ? "open" : "closed"}!!
+
+  const [nextOpenDay, setNextOpenDay] = useState(null);
+
+  useEffect(() => {
+
+
+    let today = new Date();
+    let hours = today.getHours();
+    let minutes = today.getMinutes();
+    let hoursStr = hours.toString();
+    let minutesStr = minutes.toString();
+    let currentTimeStr = hours + "." + minutes;
+    let currentTime = parseInt(currentTimeStr);
+
+    let weekday = today.getUTCDay();
+
+    let openingTime;
+    let closingTime;
+
+    openingTime = parseInt(item.opensMonday);
+    closingTime = parseInt(item.closesMonday);
+
+    /*
+    switch(weekday) {
+      case '0':
+        openingTime = parseInt(item.opensMonday);
+        closingTime = parseInt(item.closesMonday);
+
+        if (openingTime.localeCompare('-')==0) {
+          setIsOpen(false);
+          setNextOpenDay(1);
+        } else if ( openingTime-currentTime <= 0
+                    && closingTime-currentTime >= 0) {
+          setIsOpen(true);
+          setNextOpenDay(0);
+        } else if ( openingTime-currentTime > 0 ) {
+          setIsOpen(false);
+          setNextOpenDay(0);
+        } else if ( closingTime-currentTime < 0 ) {
+          setIsOpen(false);
+          setNextOpenDay(1);
+        }
+        break;
+      case '1':
+        openingTime = parseInt(item.opensTuesday);
+        closingTime = parseInt(item.closesTuesday);
+
+        if (openingTime.localeCompare('-')==0) {
+          setIsOpen(false);
+          setNextOpenDay(2);
+        } else if ( openingTime-currentTime <= 0
+                  && closingTime-currentTime >= 0) {
+          setIsOpen(true);
+          setNextOpenDay(1);
+        } else if ( openingTime-currentTime > 0 ) {
+          setIsOpen(false);
+          setNextOpenDay(1);
+        } else if ( closingTime-currentTime < 0 ) {
+          setIsOpen(false);
+          setNextOpenDay(2);
+        }
+        break;
+      case '2':
+        openingTime = parseInt(item.opensWednesday);
+        closingTime = parseInt(item.closesWednesday);
+
+        if (openingTime.localeCompare('-')==0) {
+          setIsOpen(false);
+          setNextOpenDay(3);
+        } else if ( openingTime-currentTime <= 0
+                    && closingTime-currentTime >= 0) {
+          setIsOpen(true);
+          setNextOpenDay(2);
+        } else if ( openingTime-currentTime > 0 ) {
+          setIsOpen(false);
+          setNextOpenDay(2);
+        } else if ( closingTime-currentTime < 0 ) {
+          setIsOpen(false);
+          setNextOpenDay(3);
+        }
+        break;
+      case '3':
+        openingTime = parseInt(item.opensThursday);
+        closingTime = parseInt(item.closesThursday);
+
+        if (openingTime.localeCompare('-')==0) {
+          setIsOpen(false);
+          setNextOpenDay(4);
+        } else if ( openingTime-currentTime <= 0
+                    && closingTime-currentTime >= 0) {
+          setIsOpen(true);
+          setNextOpenDay(3);
+        } else if ( openingTime-currentTime > 0 ) {
+          setIsOpen(false);
+          setNextOpenDay(3);
+        } else if ( closingTime-currentTime < 0 ) {
+          setIsOpen(false);
+          setNextOpenDay(4);
+        }
+        break;
+      case '4':
+        openingTime = parseInt(item.opensFriday);
+        closingTime = parseInt(item.closesFriday);
+
+        if (openingTime.localeCompare('-')==0) {
+          setIsOpen(false);
+          setNextOpenDay(0);
+        } else if ( openingTime-currentTime <= 0
+                    && closingTime-currentTime >= 0) {
+          setIsOpen(true);
+          setNextOpenDay(4);
+        } else if ( openingTime-currentTime > 0 ) {
+          setIsOpen(false);
+          setNextOpenDay(4);
+        } else if ( closingTime-currentTime < 0 ) {
+          setIsOpen(false);
+          setNextOpenDay(0);
+        }
+        break;
+      default:
+        setIsOpen(false);
+        setNextOpenDay(0);
+    }
+
+    // setNextOpenDay(weekday);
+  }, []);
+  */
+
   return (
     <View style={styles.contactSection}>
       <View
@@ -229,13 +400,29 @@ const Contact = ({item}) => {
       />
 
       <Text style={[ styles.contactDescription ]}>
-        { name } {isOpen ? "open" : "closed"}
+        { name } { isOpen ? "open" : "closed"}
       </Text>
       <Text style={[ styles.contactDescription ]}>
-        {  }
+        Nächste Öffnungszeit: { nextOpenDay }
       </Text>
 
       <View style={styles.buttonContainer}>
+        /*
+        <Image
+          source={require('./ukrainian-flag.png')}
+          style={ isFirstSection ? styles.flag : styles.noFlag }
+        />
+        */
+        
+        <Button
+          title="Press me"
+          color="#f194ff"
+          onPress={() => {
+            isOpen ? setIsOpen(false) : setIsOpen(true);
+          }
+        />
+
+        /*
         <Icon.Button
           name= { isOpen ? "call-outline" : "alarm-sharp" }
           onPress={() => {
@@ -253,6 +440,8 @@ const Contact = ({item}) => {
           })}
 
         >
+        */
+
           <Text style={styles.buttonText}>
             { isOpen ? "Anrufen" : "Erinner mich" }
           </Text>
@@ -266,6 +455,31 @@ const Contact = ({item}) => {
 const App = () => {
 
   const backgroundStyle = { backgroundColor: 'rgba(247,247,247,1.0)' };
+  // const [date, setDate] = useState(null);
+
+  /*
+  useEffect(() => {
+    let today = new Date();
+    let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    setDate(date);
+  }, []);
+
+
+  let weekday;
+  let currentTime;
+
+  useEffect(() => {
+    let today = new Date();
+    let hours = today.getHours();
+    let minutes = today.getMinutes();
+    let hoursStr = hours.toString();
+    let minutesStr = minutes.toString();
+    let currentTimeStr = hours + "." + minutes;
+
+    currentTime = parseInt(currentTimeStr);
+    weekday = today.getUTCDay();
+  }, []);
+  */
 
   return (
     <SafeAreaView style={backgroundStyle}>
